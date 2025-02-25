5c733f190e3d55082190763f918e28ecac33a04c
PedroPC <pedropootchan55656@gmail.com>
2025-02-25 09:56:40 -0600

    se creo la table de login con sus atributos

---
+++ upload_system.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: upload_system
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_login`
+--
+
+DROP TABLE IF EXISTS `user_login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_login` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `name_user` varchar(50) DEFAULT NULL,
+  `user_email` varchar(250) DEFAULT NULL,
+  `user_password` varchar(250) DEFAULT NULL,
+  `registration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_login`
+--
+
+LOCK TABLES `user_login` WRITE;
+/*!40000 ALTER TABLE `user_login` DISABLE KEYS */;
+INSERT INTO `user_login` VALUES (1,'Juan Pérez','juan.perez@email.com','contraseña1','2025-02-25 08:58:48'),(2,'Ana Gómez','ana.gomez@email.com','contraseña2','2025-02-25 08:58:48'),(3,'Carlos López','carlos.lopez@email.com','contraseña3','2025-02-25 08:58:48'),(4,'Laura Martínez','laura.martinez@email.com','contraseña4','2025-02-25 08:58:48'),(5,'Pedro Sánchez','pedro.sanchez@email.com','contraseña5','2025-02-25 08:58:48'),(6,'Isabel Díaz','isabel.diaz@email.com','contraseña6','2025-02-25 08:58:48'),(7,'Luis Rodríguez','luis.rodriguez@email.com','contraseña7','2025-02-25 08:58:48'),(8,'Marta Fernández','marta.fernandez@email.com','contraseña8','2025-02-25 08:58:48'),(9,'David Ruiz','david.ruiz@email.com','contraseña9','2025-02-25 08:58:48'),(10,'Elena Torres','elena.torres@email.com','contraseña10','2025-02-25 08:58:48'),(11,'Pedro Poot','pedro.poot@gmail.com','contraseña11','2025-02-25 09:39:00');
+/*!40000 ALTER TABLE `user_login` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-25  9:48:49

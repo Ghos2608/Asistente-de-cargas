26567c14fe3d63629cbeae0291065a0b6080eb74
PedroPC <pedropootchan55656@gmail.com>
2025-02-27 17:12:45 -0600

    se ha avando em ambas ventana, login y login up

--- WPF_Desarrollo/WPFApp1/WPFApp1/obj/Debug/net8.0-windows/WPFApp1.AssemblyInfo.cs
+++ WPF_Desarrollo/WPFApp1/WPFApp1/obj/Debug/net8.0-windows/WPFApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38da5a26834b6cee5cc1a318624a4907e454c96f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
